cbdfbe1cd151e9c20ee348ace07fffbe9727ed01
Joseph Jackson <joseph.jackson@MacBook-Pro.local>
2019-07-17 10:47:49 -0400

    created page with request URL from mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { REQUEST } from '../data/mockRequest';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Web Socket Viewer';
+  version = '0.1';
+
+  request = REQUEST;
 }
