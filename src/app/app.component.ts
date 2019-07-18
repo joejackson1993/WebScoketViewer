0e5f848a2556f79985efa2868ddb66a71e8e557f
Joseph Jackson <joseph.jackson@MacBook-Pro.local>
2019-07-18 14:42:19 -0400

    Added file upload fiunctionallity and edited css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,43 @@
 import { Component, ViewChild } from '@angular/core';
-import { REQUEST } from '../data/mockRequest';
+//import { REQUEST } from '../data/mockRequest';
 import { MatTableDataSource, MatSort } from '@angular/material';
 import * as moment from 'moment';
 
 // probably need to pull this out into a service at some point and actually contruct the right objects from the json.
-const FRAME_DATA: any[] = formatFrames(REQUEST._frames);
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'Web Socket Viewer';
-  version = '0.1';
-  request = REQUEST;
+  version = '1.0';
+  request: any;
   displayedColumns = ['time', 'type', 'channel', 'clientId', 'successful', 'subscription', 'text'];
-  dataSource = new MatTableDataSource(FRAME_DATA);
+  dataSource = new MatTableDataSource([{'': ''}]);
+  selectedFile: File;
+  fileContent: string;
 
-  @ViewChild(MatSort) sort: MatSort;
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsText(this.selectedFile, 'UTF-8');
+    fileReader.onload = () => {
+
+      // need to find out the best way to handle multiple requests...
+      // maybe just duplcate the table for each results from JSON.
+      this.request = JSON.parse(fileReader.result as string)[0];
+      const FRAME_DATA: any[] = formatFrames(this.request._frames);
+      this.dataSource = new MatTableDataSource(FRAME_DATA);
+    };
+    fileReader.onerror = (error) => {
+      console.log(error);
+      alert('There was an error uploading the file');
+    };
   }
 }
 
